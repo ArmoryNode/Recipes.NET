3ba64d98648f94644fad9de17b2877e2c81ffb5e
ArmoryNode <andy@shapemydistrict.com>
2023-02-09 10:07:56 -0800

    Changed project and solution names to use "Recipes.NET" instead of "RecipesDotNet"

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -2,18 +2,18 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using RecipesDotNet.Client;
+using Recipes.NET.Client;
 using System.Text.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("RecipesDotNet.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+builder.Services.AddHttpClient("Recipes.NET.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("RecipesDotNet.ServerAPI"));
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Recipes.NET.ServerAPI"));
 
 builder.Services.AddBlazoredLocalStorageAsSingleton(config =>
 {
