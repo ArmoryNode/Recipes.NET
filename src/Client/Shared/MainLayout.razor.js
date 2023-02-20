e6ba2d5ddba3caaf1eb5de8fed3f875597feb0fc
ArmoryNode <andy@shapemydistrict.com>
2023-02-19 16:17:43 -0800

    Added generated css and js for razor files to .gitignore

--- src/Client/Shared/MainLayout.razor.js
+++
@@ -1,8 +0,0 @@
-export function initializeDarkModePreference(key) {
-    if (localStorage.getItem(key) === null) {
-        const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-        const enableDarkMode = darkModeQuery.matches.toString();
-        localStorage.setItem(key, enableDarkMode.toString());
-    }
-}
-//# sourceMappingURL=MainLayout.razor.js.map
